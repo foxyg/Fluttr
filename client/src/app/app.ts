62689b5f70e02d0cdf1c64e4f8ad44cc93d1b916
Tobi <layantobi@gmail.com>
2025-12-30 09:08:27 +0100

    Routing Implentation

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,48 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { SIGNAL } from '@angular/core/primitives/signals';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
-
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  
-  protected title = "FluttR";
-  protected members = signal<User[]>([]);
+export class App {
+  protected router = inject(Router)
 
- async ngOnInit() {
-    this.members.set(await this.getMembers())
-    this.setCurrentUser();
-  }
 
-  setCurrentUser(){
-    const userString = localStorage.getItem('user')
-    if(!userString) return;
+  //  async ngOnInit() {
+  //     this.members.set(await this.getMembers())
+  //   }
 
-    const user = JSON.parse(userString)
+  // async getMembers(){
+  //   try{
+  //     return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+  //   }catch (error){
+  //     console.log(error)
+  //     throw error;
+  //   }
 
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers(){
-    try{
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    }catch (error){
-      console.log(error)
-      throw error;
-    }
-    
-  }
+  // }
 
 }

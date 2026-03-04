fd21d3312b040e1017e667e9d1ec49e39c50fc35
Tobi <layantobi@gmail.com>
2026-03-04 17:16:40 +0100

    SignalR

--- client/src/core/services/presence-service.ts
+++ client/src/core/services/presence-service.ts
@@ -39,7 +39,7 @@ export class PresenceService {
 
     this.hubConnection.on('NewMessageReceived', (message: Message) => {
       this.toast.info(message.senderDisplayName + ' has sent you a new message', 
-        10000, message.senderImageUrl, `/members/${message.senderId}/messages`);
+        20000, message.senderImageUrl, `/members/${message.senderId}/messages`);
     })
   }
 
